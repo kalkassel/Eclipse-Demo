a81f49160032a70f90c19e06c2f5b32def2f654c
KAHSAY <kkhsol@gmail.com>
2020-12-18 20:03:30 -0500

    As a developer this is my first commited task from eclipce to github.

--- src/LabExercise/GitOnEclipce.java
+++ src/LabExercise/GitOnEclipce.java
@@ -3,11 +3,71 @@ package LabExercise;
  * / Create two class Employee and EmployeeTest. where Employee is the public class and 
  * has four variables called name, age, designation and salary. 
   */
+
+//This is the class (a class which is not the source file class name)
+class EmployeeTest1 {
+	
+	// declaring instant variables.
+	String name;
+	int age;
+	String designation;
+	double salary;
+	
+	// The constructor which have the same name as the class above.
+	public EmployeeTest1(String empName) {
+		name=empName;
+	}
+	
+	// Assigning the value of the variable age using getters.
+	public int getAge() {
+		return age;
+	}
+	
+	// Assigning the value of the variable age using setters.
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	// Assigning variable designation as method . 
+	public void empDesignation(String designation) {
+		this.designation=designation;
+	}
+	
+	// Assigning variable salary as method.	
+	public void empSalary(double salary) {
+		this.salary=salary;
+	}
+	
+	// Displaying the employee information using method 
+	public void displayEmpInfo() {
+		System.out.println("Name: "+ name);
+		System.out.println("Age: "+ age);
+		System.out.println("Designation: "+ designation);
+		System.out.println("Salary: "+ salary);
+	}
+	
+}
+
 public class GitOnEclipce {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 
+		// Declaring the employee object using the constructor class 
+				EmployeeTest1 empOne = new EmployeeTest1("John_Smith");
+				EmployeeTest1 empTwo = new EmployeeTest1("William_Gibbsen");
+				
+				// Accessing the employee info using object declared for employee one. 
+				empOne.setAge(34);
+				empOne.empDesignation("Senior Engineer and Devoloper.");
+				empOne.empSalary(250000.0);
+				empOne.displayEmpInfo();
+				System.out.println("--------======-------------");
+				
+				// Accessing the employee information using object declared for employee two.
+				empTwo.setAge(36);
+				empTwo.empDesignation("Engineer and Devoloper.");
+				empTwo.empSalary(150000.0);
+				empTwo.displayEmpInfo();
 	}
 
 }
